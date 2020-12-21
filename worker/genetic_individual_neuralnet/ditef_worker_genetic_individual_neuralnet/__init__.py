227f2ad7f44f68a89d600027a6d2215905615b0b
Hendrik <git@h3ndrk.de>
2020-12-21 14:55:25 +0100

    Fix log output

--- worker/genetic_individual_neuralnet/ditef_worker_genetic_individual_neuralnet/__init__.py
+++ worker/genetic_individual_neuralnet/ditef_worker_genetic_individual_neuralnet/__init__.py
@@ -316,7 +316,7 @@ def compiledNN_average_distance(model, model_path, verification_dataset, configu
             predictions_tensorflow - predictions_compiled_nn,
         ).mean()
         if numpy.isnan(mean_squared_error) or numpy.isinf(mean_squared_error):
-            raise RuntimeError('CompiledNN check mean squared error was NaN')
+            raise RuntimeError('CompiledNN check mean squared error was NaN or Infinity')
         print('CompiledNN check finished')
         return float(mean_squared_error)
     print('CompiledNN check finished')
