518b0706f96a269f358dd5f5e973ef5d1e61e46c
Hendrik <git@h3ndrk.de>
2020-12-21 14:14:08 +0100

    Add docker files

--- worker/genetic_individual_neuralnet/build.sh
+++ worker/genetic_individual_neuralnet/build.sh
@@ -6,5 +6,4 @@ set -e -x
 cd "$(dirname "$0")"
 
 # build neural network individual worker
-#  -f generic/distributed_task_execution_framework/worker/genetic_individual_neuralnet/Dockerfile
-docker build -t hulks-genetic-individual-neuralnet -f ./Dockerfile ../../../../
+docker build -t hulks-genetic-individual-neuralnet -f ./Dockerfile ../
